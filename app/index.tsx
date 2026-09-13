2b6c2d7638ed3a9001c59f2f5e85b355fdc59f88
ibopirjasi-ship-it <ibotiran@gmail.com>
2026-09-13 20:23:05 +0200

    Create index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hap pas Hapi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ffffff",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+});
